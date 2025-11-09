65181c1cbf7f1b376a52cb653ec00bc4a0d3107c
bj-mandim <beatrizjuliomandim@gmail.com>
2025-11-08 23:51:44 -0300

    adicionando imagens para produção

--- src/data/dataEquipe.tsx
+++ src/data/dataEquipe.tsx
@@ -2,51 +2,51 @@ export const equipeDiretoria = [
   {
     nome: "Bárbara",
     cargo: "Presidente",
-    imagem: "/public/diretora-geral.jpeg",
+    imagem: "/diretora-geral.jpeg",
   },
   {
     nome: "Thais",
     cargo: "Vice-Presidente",
-    imagem: "/public/thais-vice-diretora.jpeg",
+    imagem: "/thais-vice-diretora.jpeg",
   },
   {
     nome: "Luana",
     cargo: "Diretora de Marketing",
-    imagem: "/public/luana-diretora-de-marketing.jpeg",
+    imagem: "/luana-diretora-de-marketing.jpeg",
   },
   {
     nome: "Emanuelly",
     cargo: "Vice-Diretora de Marketing",
-    imagem: "/public/vice-diretora-marketing-emanuely.jpeg",
+    imagem: "/vice-diretora-marketing-emanuely.jpeg",
   },
   {
     nome: "Rayani",
     cargo: "Diretora de Eventos",
-    imagem: "/public/diretora-de-eventos-rayani.jpeg",
+    imagem: "/diretora-de-eventos-rayani.jpeg",
   },
   {
     nome: "Isabelly",
     cargo: "Diretora Social",
-    imagem: "/public/Isabelly-diretora-social.jpeg",
+    imagem: "/Isabelly-diretora-social.jpeg",
   },
   {
     nome: "Janaina",
     cargo: "Vice-Diretora Social",
-    imagem: "/public/janaina-vice-diretora-social.jpeg",
+    imagem: "/janaina-vice-diretora-social.jpeg",
   },
   {
     nome: "Paolla",
     cargo: "Diretora Acadêmica",
-    imagem: "/public/paola-diretora-academica.jpeg",
+    imagem: "/paola-diretora-academica.jpeg",
   },
   {
     nome: "Natalia",
     cargo: "Diertora de Produtos",
-    imagem: "/public/natalia-diretora-de-produtos.jpeg",
+    imagem: "/natalia-diretora-de-produtos.jpeg",
   },
   {
     nome: "Sara",
     cargo: "Secretária",
-    imagem: "/public/sara-secretaria.jpeg",
+    imagem: "/sara-secretaria.jpeg",
   },
 ];
